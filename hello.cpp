1bef93f1eaf56e58a97fc816d8c4a88e0bf2b1b2
sundaresan765 <88373165+sundaresan765@users.noreply.github.com>
2024-07-28 21:20:45 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream> // Including the input/output stream library to enable input and output operations.
+
+using namespace std; // Using the standard namespace to avoid having to prepend 'std::' to standard C++ objects and functions.
+
+int main (){ // The starting point of the program where execution begins.
+
+    std::cout << "Hello DSA" << std::endl; // Outputting the string "Hello DSA" followed by a newline character to the console.
+
+} // End of the main function.
