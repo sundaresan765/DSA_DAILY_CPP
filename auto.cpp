0f430e4cb398f961d5a0d38eb069368b75f5d321
sundaresan765 <88373165+sundaresan765@users.noreply.github.com>
2024-08-10 18:50:10 +0530

    Add files via upload

---
+++ auto.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    auto a=67;
+    auto b=6.7;
+    auto c="fjfj";
+    auto d='s';
+    std::cout<<a<<std::endl;
+    std::cout<<b<<std::endl;
+    std::cout<<c<<std::endl;
+    std::cout<<d<<std::endl;
+
+}
